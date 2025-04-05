53a648f7b08e0efcdca2b01ed08d52bacf991cc8
darkhacker1906 <singhabhinandan634@gmail.com>
2025-04-05 20:04:27 +0530

    Added the router and pinia for state-management

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    This is my Homepage  component.
-    
-  </div>
-</template>
-<script setup>
-import {useCounterStore} from "../store/counterStore.js"
-
-</script>
-
-<style scoped>
-
-</style>
+    <div>
+      This is my Homepage component.
+  
+      <p>Count: {{ counter.count }}</p>
+      <button @click="counter.increment">Increment</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useCounterStore } from "../store/counterStore.js"
+  
+  const counter = useCounterStore()
+  </script>
+  
+  <style scoped>
+  button {
+    margin-top: 10px;
+  }
+  </style>
+  
